62533ebf97877ab560bbe42989f3805cca81ab14
pritikmshaw <pritikmshaw18@gmail.com>
2020-06-03 04:23:29 +0530

    Create mean and deviation.py
    how to calculate mean

---
+++ mean and deviation.py
@@ -0,0 +1,10 @@
+'''How to calculate mean and deviation
+ of two numbers...'''
+a=int(input("Enter a no:"))
+b=int(input("Enter a no:"))
+c=float((a+b)/2)
+d1=a-c
+d2=b-c
+print(c)
+print(d1)
+print(d2)
