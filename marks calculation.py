9041f3c88d6c07fd23818a5f0849daa52a442ed2
pritikmshaw <pritikmshaw18@gmail.com>
2020-06-04 04:11:37 +0530

    Update marks calculation.py

--- marks calculation.py
+++ marks calculation.py
@@ -1,3 +1,5 @@
 '''calculating students result 
      based on the examinations'''
-n=int(input("enter the number of exams"))
+n=int(input("enter the number of exams :"))
+for i in range (1,n+1):
+    exam=int(input("enter the marks you got in exam "+str(i)+":"))
