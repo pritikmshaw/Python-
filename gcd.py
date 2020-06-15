fa38573cd35781450a9da16189941441708ab413
pritikmshaw <pritikmshaw18@gmail.com>
2020-06-15 22:57:58 +0530

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -2,7 +2,8 @@
 a = int(input('enter the first number '))
 b = int(input('enter the second number '))
 i = 1
-while(a <= i and b <= i):
+
+while(a >= i and b >= i):
     if(a%i==0 and b%i==0):
         gcd = i
     i = i+1
