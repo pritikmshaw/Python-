e84cf0ec14b29d8ce39dfcbb51273261f03efe17
pritikmshaw <pritikmshaw18@gmail.com>
2020-09-12 01:07:03 +0530

    vowel

---
+++ vowelconsonant.py
@@ -0,0 +1,11 @@
+# a character is a vowel or consonant
+
+ch = input("enter a word")
+if ((ch == 'a' or ch == 'e' or ch=='i' or ch=='o' or ch=='u' ) or (ch=='A' or ch=='E' or ch=='I' or ch =='O' or ch=='U')):
+    {
+        print ("vowel")
+    }
+else:
+    {
+        print("consonat")
+    }
