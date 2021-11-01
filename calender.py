3d8aea28a100f0653177ff4d0c4a73516c507e38
pritikmshaw <pritikmshaw18@gmail.com>
2021-11-01 21:58:37 +0530

    calender

---
+++ calender.py
@@ -0,0 +1,7 @@
+# First import the calendar module
+import calendar
+# ask of month and year
+yy=int(input("enter year : "))
+mm=int(input("enter month : "))
+# display the calendar
+print(calendar.month(yy,mm))
