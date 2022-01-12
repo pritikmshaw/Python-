1fd67fbd1e93e146333ae9df80dca71b3d460348
pritikmshaw <pritikmshaw18@gmail.com>
2022-01-13 00:37:59 +0530

    files addes

---
+++ largest_of_3.py
@@ -0,0 +1,13 @@
+# largest of 3 nos 
+a = int(input("enter a no: "))
+b = int(input("enter a no: "))
+c = int(input("enter a no: "))
+
+if(a>b and a>c):
+    print (" a is the greatest")
+
+if(b>a and b>c):
+    print("b is the greatest")
+
+if(c>a and c>b):
+    print("c is the greatest")
---
+++ odd_even.py
@@ -0,0 +1,6 @@
+#print weather the number is odd or even
+num = int(input("enter a no : "))
+if(num%2==0):
+    print("even")
+else:
+    print("odd")
